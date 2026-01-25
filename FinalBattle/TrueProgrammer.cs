5244a95058902d42fe3884fefed9e71269fc2196
Markus <markus15b@gmail.com>
2026-01-25 16:54:59 +0100

    Added some color to the prompt

--- FinalBattle/TrueProgrammer.cs
+++ FinalBattle/TrueProgrammer.cs
@@ -4,8 +4,14 @@ public class TheTrueProgrammer : Character {
     private string? name;
 
     public TheTrueProgrammer() {
+        ConsoleColor previousColor = Console.ForegroundColor;
+        Console.ForegroundColor =  ConsoleColor.Gray;
         Console.Write("You are the True Programmer, what is your name: ");
+        Console.ForegroundColor = previousColor;
+
+        Console.ForegroundColor = ConsoleColor.Cyan;
         name = Console.ReadLine();
+        Console.ForegroundColor = previousColor;
     }
 
     public override string Name => name ?? string.Empty;
